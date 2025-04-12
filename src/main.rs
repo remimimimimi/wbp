91f1268afe3a1c01718a51c09c046b3479029f98
remimimimimi <remimimimimi@protonmail.com>
2025-04-12 20:01:58 +0300

    Migrate to softbuffer

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,20 @@
 use crate::painting::PixelBuffer;
 
-use std::fs::File;
+use std::fs::{self, File};
 use std::io::Read;
-use std::time::Instant;
+use std::num::NonZeroU32;
+use std::sync::{mpsc, Arc, Mutex};
+use std::time::UNIX_EPOCH;
 
-use pixels::{Error, Pixels, SurfaceTexture};
 use tiny_skia::Pixmap;
+
 use tracing::*;
 use tracing_subscriber::FmtSubscriber;
-use winit::dpi::LogicalSize;
-use winit::event::{Event, WindowEvent};
-use winit::event_loop::EventLoop;
-use winit::keyboard::KeyCode;
-use winit::window::WindowBuilder;
-use winit_input_helper::WinitInputHelper;
+
+use winit::event::{Event, KeyEvent, WindowEvent};
+use winit::event_loop::{ControlFlow, EventLoop};
+use winit::keyboard::{Key, NamedKey};
+use winit::window::Window;
 
 pub mod css;
 pub mod dom;
@@ -21,9 +22,10 @@ pub mod html;
 pub mod layout;
 pub mod painting;
 pub mod style;
+pub mod winit_app;
 
-const WIDTH: u32 = 500;
-const HEIGHT: u32 = 500;
+const HTML_FILE_PATH: &str = "test.html";
+const CSS_FILE_PATH: &str = "test.css";
 
 pub fn initialize_logging() {
     // a builder for `FmtSubscriber`.
@@ -37,7 +39,7 @@ pub fn initialize_logging() {
     tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
 }
 
-pub fn read_source(filename: String) -> String {
+pub fn read_source(filename: &str) -> String {
     let mut s = String::new();
     File::open(filename)
         .unwrap()
@@ -46,93 +48,188 @@ pub fn read_source(filename: String) -> String {
     s
 }
 
-fn main() -> Result<(), Error> {
-    initialize_logging();
-
-    let html = read_source("test.html".into());
-    let css = read_source("test.css".into());
-
-    let root_node = html::parse(html);
-    let stylesheet = css::parse(css);
-    let style_root = style::style_tree(&root_node, &stylesheet);
-
-    let event_loop = EventLoop::new().unwrap();
-    let mut input = WinitInputHelper::new();
-    let window = {
-        let size = LogicalSize::new(WIDTH as f64, HEIGHT as f64);
-        WindowBuilder::new()
-            .with_title("Hello tiny-skia")
-            .with_inner_size(size)
-            .with_min_inner_size(size)
-            .build(&event_loop)
-            .unwrap()
-    };
-
-    let mut pixels = {
-        let window_size = window.inner_size();
-        let surface_texture = SurfaceTexture::new(window_size.width, window_size.height, &window);
-
-        Pixels::new(WIDTH, HEIGHT, surface_texture)?
-    };
-
-    let mut drawing = Pixmap::new(WIDTH, HEIGHT).unwrap();
-    let _now = Instant::now();
-
-    let res = event_loop.run(|event, elwt| {
-        // Draw the current frame
-        if let Event::WindowEvent {
-            event: WindowEvent::RedrawRequested,
-            ..
-        } = event
-        {
-            pixels.frame_mut().copy_from_slice(drawing.data());
-            if let Err(err) = pixels.render() {
-                log_error("pixels.render", err);
-                elwt.exit();
-                return;
-            }
-        }
-
-        // Handle input events
-        if input.update(&event) {
-            // Close events
-            if input.key_pressed(KeyCode::Escape) || input.close_requested() {
-                elwt.exit();
-                return;
-            }
-
-            // Resize the window
-            if let Some(size) = input.window_resized() {
-                if let Err(err) = pixels.resize_surface(size.width, size.height) {
-                    log_error("pixels.resize_surface", err);
-                    elwt.exit();
-                    return;
-                }
-            }
-
-            // Update internal state and request a redraw
-            let layout_root = layout::layout_tree(
-                &style_root,
-                layout::Dimensions {
-                    // TODO: Update when actual width and height updates.
-                    content: layout::Rect {
-                        width: WIDTH as f32,
-                        height: HEIGHT as f32,
-                        ..Default::default()
-                    },
-                    ..Default::default()
-                },
-            );
-            drawing.paint(&layout_root);
-            window.request_redraw();
-        }
-    });
-    res.map_err(|e| Error::UserDefined(Box::new(e)))
+pub fn file_modified_time_in_seconds(path: &str) -> u64 {
+    fs::metadata(path)
+        .unwrap()
+        .modified()
+        .unwrap()
+        .duration_since(UNIX_EPOCH)
+        .unwrap()
+        .as_secs()
 }
 
-fn log_error<E: std::error::Error + 'static>(method_name: &str, err: E) {
-    error!("{method_name}() failed: {err}");
-    // for source in err.sources().skip(1) {
-    //     error!("  Caused by: {source}");
-    // }
+type Surface = softbuffer::Surface<Arc<Window>, Arc<Window>>;
+
+fn render_thread(
+    window: Arc<Window>,
+    do_render: mpsc::Receiver<Arc<Mutex<Surface>>>,
+    done: mpsc::Sender<()>,
+) {
+    loop {
+        debug!("waiting for render...");
+        let Ok(surface) = do_render.recv() else {
+            info!("main thread destroyed");
+            break;
+        };
+
+        // Perform the rendering.
+        let mut surface = surface.lock().unwrap();
+        if let (Some(width), Some(height)) = {
+            let size = window.inner_size();
+            debug!("got size: {size:?}");
+            (NonZeroU32::new(size.width), NonZeroU32::new(size.height))
+        } {
+            debug!("resizing...");
+            surface.resize(width, height).unwrap();
+
+            let mut buffer = surface.buffer_mut().unwrap();
+
+            if let Some(mut pixmap) = Pixmap::new(width.get(), height.get()) {
+                pixmap.fill(tiny_skia::Color::WHITE);
+
+                let html = read_source(HTML_FILE_PATH);
+                let css = read_source(CSS_FILE_PATH);
+
+                let root_node = html::parse(html.clone());
+                let stylesheet = css::parse(css.clone());
+                let style_root = style::style_tree(&root_node, &stylesheet);
+
+                // Update internal state and request a redraw
+                let layout_root = layout::layout_tree(
+                    &style_root,
+                    layout::Dimensions {
+                        content: layout::Rect {
+                            width: width.get() as f32,
+                            height: height.get() as f32,
+                            ..Default::default()
+                        },
+                        ..Default::default()
+                    },
+                );
+                pixmap.paint(&layout_root);
+
+                // Copy tiny_skia pixmap to the window buffer
+                let bytes = pixmap.data();
+
+                assert!(
+                    bytes.len() % 4 == 0,
+                    "The length of the slice must be divisible by 4"
+                );
+
+                for (i, pix) in bytes.chunks(4).enumerate() {
+                    // Since we already checked that lenght is
+                    // divisible by 4 we can safely assume
+                    // that each chunk will be slice of 4
+                    // elements.
+                    let pix_assembled = u32::from_be_bytes(
+                        <[u8; 4]>::try_from([0, pix[0], pix[1], pix[2]])
+                            .expect("Ups, I did it again..."),
+                    );
+                    // Otherwise on window resize we will get panic, since actual window buffer will change, but pixmap won't.
+                    if i < buffer.len() {
+                        buffer[i] = pix_assembled;
+                    }
+                }
+            } else {
+                // FIXME: Should actually not occur due to
+                // check in the outer if let (width and height
+                // are nonzero).
+                warn!("Cannot create pixel map");
+            }
+
+            debug!("presenting...");
+            buffer.present().unwrap();
+        }
+
+        // We're done, tell the main thread to keep going.
+        done.send(()).ok();
+    }
+}
+
+pub fn entry(event_loop: EventLoop<()>) {
+    let mut html_last_changed = dbg!(file_modified_time_in_seconds(HTML_FILE_PATH));
+    let mut css_last_changed = dbg!(file_modified_time_in_seconds(CSS_FILE_PATH));
+
+    let app = winit_app::WinitAppBuilder::with_init(
+        |elwt| {
+            let attributes = Window::default_attributes();
+            let window = Arc::new(elwt.create_window(attributes).unwrap());
+
+            let context = softbuffer::Context::new(window.clone()).unwrap();
+
+            // Spawn a thread to handle rendering for this specific surface. The channels will
+            // be closed and the thread will be stopped whenever this surface (the returned
+            // context below) is dropped, so that it can all be recreated again (on Android)
+            // when a new surface is created.
+            let (start_render, do_render) = mpsc::channel();
+            let (render_done, finish_render) = mpsc::channel();
+            debug!("starting thread...");
+            std::thread::spawn({
+                let window = window.clone();
+                move || render_thread(window, do_render, render_done)
+            });
+
+            (window, context, start_render, finish_render)
+        },
+        |_elwt, (window, context, _start_render, _finish_render)| {
+            debug!("making surface...");
+            Arc::new(Mutex::new(
+                softbuffer::Surface::new(context, window.clone()).unwrap(),
+            ))
+        },
+    )
+    .with_event_handler(move |state, surface, event, elwt| {
+        let (window, _context, start_render, finish_render) = state;
+        elwt.set_control_flow(ControlFlow::Wait);
+
+        // Reload files if they changed on drive.
+        let html_last_changed_now = file_modified_time_in_seconds(HTML_FILE_PATH);
+        let css_last_changed_now = file_modified_time_in_seconds(CSS_FILE_PATH);
+
+        if html_last_changed != html_last_changed_now || css_last_changed != css_last_changed_now {
+            html_last_changed = html_last_changed_now;
+            css_last_changed = css_last_changed_now;
+            window.request_redraw();
+        }
+
+        match event {
+            Event::WindowEvent {
+                window_id,
+                event: WindowEvent::RedrawRequested,
+            } if window_id == window.id() => {
+                debug!("Redraw requested");
+                let Some(surface) = surface else {
+                    error!("RedrawRequested fired before Resumed or after Suspended");
+                    return;
+                };
+                // Start the render and then finish it.
+                start_render.send(surface.clone()).unwrap();
+                finish_render.recv().unwrap();
+            }
+            Event::WindowEvent {
+                event:
+                    WindowEvent::CloseRequested
+                    | WindowEvent::KeyboardInput {
+                        event:
+                            KeyEvent {
+                                logical_key: Key::Named(NamedKey::Escape),
+                                ..
+                            },
+                        ..
+                    },
+                window_id,
+            } if window_id == window.id() => {
+                elwt.exit();
+            }
+            _ => {}
+        }
+    });
+
+    winit_app::run_app(event_loop, app);
+}
+
+fn main() {
+    use winit::event_loop::EventLoop;
+    initialize_logging();
+    entry(EventLoop::new().unwrap())
 }
